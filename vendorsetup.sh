db47147a5a8bda24ad1a375f4796ec04823984f3
Ckicker <rkuznicov757si@gmail.com>
2026-06-18 21:20:23 +1000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_frost-user
-add_lunch_combo omni_frost-userdebug
-add_lunch_combo omni_frost-eng
+# Nothing here — use COMMON_LUNCH_CHOICES in AndroidProducts.mk
